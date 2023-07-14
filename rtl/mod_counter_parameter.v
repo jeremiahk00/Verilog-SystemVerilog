7d69c1c641f7200d7ae53604d2ee40b1f05c889d
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2023-07-14 17:11:02 -0400

    Create mod_counter_parameter.v

---
+++ rtl/mod_counter_parameter.v
@@ -0,0 +1,30 @@
+module mod_counter_hardcoded
+    #(parameter FINAL_VALUE = 9){
+    input clk,
+    input reset_n,
+    input enable,
+    output [BITS - 1:0] Q
+    };
+
+    localparam BITS  = $clog2(FINAL_VALUE);
+  
+    reg [BITS - 1: 0] Q_reg, Q_next;
+    wire done;
+
+    always @(posedge clk, negedge reset_n)
+        begin
+          if (~reset_n)
+            Q_reg <= 1'b0;
+          else if(enable)
+            Q_reg <= Q_next;
+          else
+            Q_reg <= Q_reg;
+        end
+  
+    assign done = Q_reg == FINAL_VALUE;
+    //next state logic
+    always @(*)
+        Q_next <= done?1'b0:Q_reg + 1;
+    //output logic
+    assign Q = Q_reg;
+endmodule

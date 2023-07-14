b32a774be14f4f5597e1f7dc1b25867a4356779f
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2023-07-14 19:40:40 -0400

    Update mod_counter_parameter.v

--- rtl/mod_counter_parameter.v
+++ rtl/mod_counter_parameter.v
@@ -1,10 +1,10 @@
 module mod_counter_parameter
-    #(parameter FINAL_VALUE = 9){
+    #(parameter FINAL_VALUE = 9)(
     input clk,
     input reset_n,
     input enable,
     output [BITS - 1:0] Q
-    };
+    );
 
     localparam BITS  = $clog2(FINAL_VALUE);
   

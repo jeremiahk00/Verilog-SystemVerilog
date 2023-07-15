e6b7d0c6e099b6570424425e350436b05724f956
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2023-07-14 20:50:13 -0400

    Update pwm_improved.v

--- rtl/pwm_improved.v
+++ rtl/pwm_improved.v
@@ -1,5 +1,5 @@
 module pwm_improved
-  #(parameter R = 8, TICKET_BITS = 15)(
+  #(parameter R = 8, TIMER_BITS = 15)(
     input clk,
     input reset_n,
     input [R : 0] duty, //we want to have enough bits to include all the increments in case duty = 1
@@ -8,19 +8,45 @@ module pwm_improved
   );
   wire tick;
   reg [R - 1: 0] Q_reg, Q_next;
-
+  /*
+  we keep these registers since comparators may be faster than how fast the bits change in q_reg so 
+  we feed the value from the comparator into another register and let it output the value 
+  synchronous to the timer ticks
+  */
+  reg d_reg, d_next; 
   always @(posedge clk, negedge reset_n)
     begin
       if(~reset_n)
-        Q_reg <= 1'b0;
+        begin
+          Q_reg <= 'b0;
+          d_reg <= 1'b0;
+        end
       else if (tick)
-        Q_reg <= Q_next;
+        begin
+          Q_reg <= Q_next;
+          d_reg <= d_next;
+        end
       else 
-        Q_reg <= Q_reg;
+        begin
+          Q_reg <= Q_reg;
+          d_reg <= d_reg;
+        end
     end
 
   always @(*)
-    Q_next <= Q_reg +1;
+    begin
+      Q_next = Q_reg +1;
+      d_next = (Q_reg < duty);
+    end
 
-  assign pwm_out = (Q_reg < duty);
+  assign pwm_out = d_reg;
+
+  //Prescalar Timer
+  timer_input #(.BITS(TIMER_BITS)) timer0 (
+    .clk(clk),
+    .reset_n(reset_n),
+    .enable(1'b1),
+    .FINAL_VALUE(FINAL_VALUE),
+    .done(tick)
+  );
 endmodule

bd6065751de9ad6f36d592dce013f3d9bc5dffc3
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2023-07-18 13:50:14 -0400

    Update fsm_counter.v

--- rtl/fsm_counter.v
+++ rtl/fsm_counter.v
@@ -40,3 +40,42 @@ module fsm_counter(
   //output logic
   assign num = state_reg;
 endmodule
+
+/*
+module fsm_counter_tb(
+
+);
+  reg clk, reset_n, en;
+  wire [2:0] num;
+
+  //instantiate dut
+
+  fsm_counter_uut0(
+    .clk(clk),
+    .reset_n(reset_n),
+    .en(en),
+    .num(num)
+  );
+
+  //generating a clock signal
+  localparam T = 10;
+  always
+    begin
+      clk = 1'b0;
+      #(T/2);
+      clk = 1'b1;
+      #(T/2);
+    end
+
+  initial begin
+    reset_n = 1'b0;
+    en = 1'b0;
+    @(negedge clk);
+    reset_n = 1'b1;
+    en = 1'b1;
+
+    repeat(20) @(negedge clk);
+    $finish;
+  end
+endmodule
+*/

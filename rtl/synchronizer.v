45275e9ab85d84f38ff1864b9a508bacf5399680
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2023-07-24 12:09:35 -0400

    Create synchronizer.v

---
+++ rtl/synchronizer.v
@@ -0,0 +1,16 @@
+module synchronizer
+  #(parameter STAGES = 2)(
+    input clk, reset_n,
+    input D,
+    output Q
+  );
+
+  reg [STAGES - 1:0] Q_reg;
+  always @(posedge clk, negedge reset_n) begin
+    if(~reset_n)
+      Q_reg <= 1'b0;
+    else
+      Q_reg <= {D, Q_reg[STAGES - 1:1]};
+  end
+  assign Q = Q_reg[0];
+endmodule
